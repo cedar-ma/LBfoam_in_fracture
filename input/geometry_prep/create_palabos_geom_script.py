5bcd196219dc57e364abd6f107aa3704e6047e95
cedar-ma <103262367+cedar-ma@users.noreply.github.com>
2023-02-17 17:24:12 -0600

    Add input folder

---
+++ input/geometry_prep/create_palabos_geom_script.py
@@ -0,0 +1,27 @@
+import subprocess
+import os
+from create_geom_for_palabos import *
+# from create_palabos_input_file import *
+from parse_input_file import *
+
+# Steps
+# 1) parse input file
+# 2) create geom for palabos
+# 3) create palabos input file
+# 4) run 1-phase sim
+# Please see python_1_phase_workflow in examples directory on how to use this
+
+input_file = 'input.yml'
+
+# 1) Process input file
+inputs = parse_input_file(input_file)
+
+if inputs['simulation type'] == '2-phase':
+    raise KeyError('Simulation type set to 2-phase...please change to 1-phase.')
+sim_directory = inputs['input output']['simulation directory']
+
+# 2) Create Palabos geometry
+print('Creating efficient geometry for Palabos...')
+create_geom_for_palabos(inputs)
+
+
